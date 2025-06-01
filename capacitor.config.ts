d7ca1837709a21e1361a610c7f979a30d370dd8e
Blake Lamb <blake.lamb31@gmail.com>
2025-06-01 14:38:51 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cruise-ducks',
+  webDir: 'www'
+};
+
+export default config;
